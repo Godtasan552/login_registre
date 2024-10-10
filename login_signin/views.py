2f9c969b2bed974934b324a3484ac64644fd5f03
Weerawat Tammung <werawat7903@gmail.com>
2024-10-10 19:12:02 +0700

    edit error delete colon and add h1 wello world

--- login_signin/views.py
+++ login_signin/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,HttpResponse
 
 def indexPage(request):
-    return render(request, 'index.html'),
+    return render(request, 'index.html')
 
 # Create your views here.
