4bd9d726f210c6d45718c16ff0f8a25124af2761
Weerawat Tammung <werawat7903@gmail.com>
2024-10-12 17:03:06 +0700

    add login and register and urls

--- login_signin/urls.py
+++ login_signin/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path("", views.indexPage, name='index'),
-    
+    path('', views.indexPage, name='index'),
+    path('login/',views.loginPage, name='login'),
+    path('register/',views.reggisterPage, name='register'),
 ]
--- login_signin/views.py
+++ login_signin/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render,HttpResponse
 def indexPage(request):
     return render(request, 'index.html')
 
-# Create your views here.
+def loginPage(request):
+    return render(request, 'login.html')
+
+def reggisterPage(request):
+    return render(request, 'register.html')
