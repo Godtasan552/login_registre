d7f0225f03da0ee12ea36f978966a3bcdddbe6fa
Weerawat Tammung <werawat7903@gmail.com>
2024-10-21 20:11:34 +0700

    set views login

--- login_signin/views.py
+++ login_signin/views.py
@@ -1,13 +1,32 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse,redirect
+from django.contrib.auth import login, authenticate
+
 
 def indexPage(request):
     return render(request, 'index.html')
 
+
 def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(
+            request,
+            username = username,
+            password = password
+        )
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+                return HttpResponse("Invalid login credentials")  # เพิ่มกรณีผู้ใช้ไม่ถูกต้อง
     return render(request, 'login.html')
+
+
 
 def registerPage(request):
     return render(request, 'register.html')
+
 
 def profilePage(request):
     return render(request, 'profile.html')
