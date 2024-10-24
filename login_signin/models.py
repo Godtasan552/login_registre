2dee6f194e9862d489a3f0f659696ed2f2adc29c
Weerawat Tammung <werawat7903@gmail.com>
2024-10-24 22:25:19 +0700

    setting redirect and set models Customuser

--- login_signin/models.py
+++ login_signin/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    birth_data = models.DateField(null=True, blank=True)
