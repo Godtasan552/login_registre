848635a4c6a06aea9003cac31b917fdcd083953a
Weerawat Tammung <werawat7903@gmail.com>
2024-10-25 00:07:09 +0700

    add messages alert and edit register

--- log_and_sign/settings.py
+++ log_and_sign/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -50,6 +51,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+MESSAGES_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 ROOT_URLCONF = 'log_and_sign.urls'
 
--- login_signin/views.py
+++ login_signin/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.utils.translation import gettext as _
 from .forms import CustomUserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 
@@ -14,13 +15,25 @@ def registerPage(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, _('สมัครสมาชิกสำเร็จ! ยินดีต้อนรับเข้าสู้ระบบ'))
             return redirect('profile')
         else:
-            form = CustomUserCreationForm()
-        return render(request, 'register.html',{'form': form})
+            messages.error(request, _('กรุณาแก้ไขข้อผิดพลาด'))
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html',{'form': form})
 
 def loginPage(request):
-    return render(request, 'login.html')
+    if request.user.is_authenticated:
+        return redirect('profile')
+    
+    if request.method. == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not
 
 def profilePage(request):
     return render(request, 'profile.html')
